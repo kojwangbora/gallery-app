fc2d06d843368806d9c09a8347fc370c06eb8e8c
kojwangbora <kojwangbora254@gmail.com>
2022-03-26 23:58:52 +0300

    create dynamic content to views

--- photos/views.py
+++ photos/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime as dt
 
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to the gallery App')
+
+def photo_of_day(request):
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
+
