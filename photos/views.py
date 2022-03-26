ecfac11991ebfdc4b9433a92726e22e27f68c552
kojwangbora <kojwangbora254@gmail.com>
2022-03-26 23:46:14 +0300

    connect app view function to a specific URL

--- photos/views.py
+++ photos/views.py
@@ -1,6 +1,6 @@
-from http.client import HTTPResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def welcome(request):
-    return HTTPResponse('Welcome to the gallery App')
+    return HttpResponse('Welcome to the gallery App')
