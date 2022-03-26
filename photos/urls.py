b9e13c6c5fff8cba544f862b85c8beb0cb2a742b
kojwangbora <kojwangbora254@gmail.com>
2022-03-27 00:03:03 +0300

    create new urlpattern for view

--- photos/urls.py
+++ photos/urls.py
@@ -2,5 +2,6 @@ from django.urls import re_path
 from . import views
 
 urlpatterns=[
-    re_path('^$',views.welcome,name='welcome')
+    re_path('^$',views.welcome,name='welcome'),
+    re_path('^today/$',views.photo_of_day,name='photoToday')
 ]
