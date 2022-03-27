b1ced214ea46073039210003b0a550fed5d8ed45
kojwangbora <kojwangbora254@gmail.com>
2022-03-27 18:33:21 +0300

    create and migrate model image

--- photos/tests.py
+++ photos/tests.py
@@ -9,7 +9,12 @@ class LocationTestClass(TestCase):
     # Set up method
     def setUp(self):
         self.westlands= Location(location_name = 'westlands', location_town ='Nairobi')
+        self.westlands.save_location()
+    
+        
     
     # Testing  instance
     def test_instance(self):
         self.assertTrue(isinstance(self.westlands,Location))
+    
+       
