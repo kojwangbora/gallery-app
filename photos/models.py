11c95af22e2055e7e98ca2547bffcc5b3d64f98a
kojwangbora <kojwangbora254@gmail.com>
2022-03-27 17:58:35 +0300

    changed meta options on location

--- photos/models.py
+++ photos/models.py
@@ -7,6 +7,21 @@ class Location(models.Model):
     location_name =models.CharField(max_length=50, blank=TRUE, default=None)
     location_town = models.CharField(max_length=25)
 
+    # def save_location(self):
+    #     self.save()
     
+    def __str__(self):
+        return self.location_name
+    
+    class Meta:
+        ordering = ['location_name']
+
+class Category(models.Model):
+    category = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.category
+
+
     
 
