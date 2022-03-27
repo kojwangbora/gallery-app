298da777872d67bac8fa947bba155dc30a63b4f9
kojwangbora <kojwangbora254@gmail.com>
2022-03-27 17:21:00 +0300

    create model class location

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from pickle import FALSE, TRUE
+
 
 # Create your models here.
+class Location(models.Model):
+    location_name =models.CharField(max_length=50, blank=TRUE, default=None)
+    location_town = models.CharField(max_length=25)
+
+    
+    
+
