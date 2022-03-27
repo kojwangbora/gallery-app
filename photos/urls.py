69d46cc1748728849f66a2ef9abe6d5936808ae9
kojwangbora <kojwangbora254@gmail.com>
2022-03-27 22:44:56 +0300

    add background image

--- photos/urls.py
+++ photos/urls.py
@@ -1,5 +1,7 @@
 from django.urls import re_path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns=[
     re_path(r'^$',views.welcome,name='welcome'),
@@ -7,3 +9,6 @@ urlpatterns=[
     re_path(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_photos,name = 'pastPhoto'),
     re_path('search/', views.search, name='search') 
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
