21a4db8f1763ad597b205768a3a90710a0fd7965
kojwangbora <kojwangbora254@gmail.com>
2022-03-27 11:06:39 +0300

    add template directory and welcome.html file

--- photos/views.py
+++ photos/views.py
@@ -4,7 +4,7 @@ import datetime as dt
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to the gallery App')
+    return render(request,'Welcome.html')
 
 def photo_of_day(request):
     date = dt.date.today()
